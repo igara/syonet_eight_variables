c986b7a4ed57aaccb20377e64262a192c100205d
igara <igara1119@gmail.com>
2022-01-30 20:58:38 +0900

    add: test file.

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,11 @@
+import * as index from '../index';
+
+describe('handler', () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
+
+  test('default', async () => {
+    expect('').toBe('');
+  });
+});
