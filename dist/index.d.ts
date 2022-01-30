64c6de0ff5582945c7dcf5fa20f51016d87a4899
igara <igara1119@gmail.com>
2022-01-31 00:40:27 +0900

    add: variables.

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,34 @@
 export declare const backupQiitaListURI = "https://api.github.com/repos/igara/qiita-export/contents/data/igara";
 export declare const backupHatenaListURI = "https://api.github.com/repos/igara/hatenablog-export/contents/data/igara1119";
+export declare type BackupHatenaList = {
+    name: string;
+    path: string;
+    sha: string;
+    size: number;
+    url: string;
+    html_url: string;
+    git_url: string;
+    download_url: string | null;
+    type: string;
+    _links: {
+        self: string;
+        git: string;
+        html: string;
+    };
+}[];
+export declare type BackupQiitaList = {
+    name: string;
+    path: string;
+    sha: string;
+    size: number;
+    url: string;
+    html_url: string;
+    git_url: string;
+    download_url: string | null;
+    type: string;
+    _links: {
+        self: string;
+        git: string;
+        html: string;
+    };
+}[];
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,37 @@ export const backupQiitaListURI =
   'https://api.github.com/repos/igara/qiita-export/contents/data/igara';
 export const backupHatenaListURI =
   'https://api.github.com/repos/igara/hatenablog-export/contents/data/igara1119';
+
+export type BackupHatenaList = {
+  name: string;
+  path: string;
+  sha: string;
+  size: number;
+  url: string;
+  html_url: string;
+  git_url: string;
+  download_url: string | null;
+  type: string;
+  _links: {
+    self: string;
+    git: string;
+    html: string;
+  };
+}[];
+
+export type BackupQiitaList = {
+  name: string;
+  path: string;
+  sha: string;
+  size: number;
+  url: string;
+  html_url: string;
+  git_url: string;
+  download_url: string | null;
+  type: string;
+  _links: {
+    self: string;
+    git: string;
+    html: string;
+  };
+}[];
